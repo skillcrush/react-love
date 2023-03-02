9be8146d299132f8eb663bf270dc0379bad89bcc
Ann <4411121+redrambles@users.noreply.github.com>
2023-03-02 13:32:06 -0500

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { Heart, Smile } from "react-feather";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    count: 0,
+    scale: 1
+  };
+
+  handleClick = () => {
+    this.setState({ count: this.state.count + 1, scale: this.state.scale + 0.075 });
+  };
+
+  render() {
+    return (
+      <main className='App'>
+        {this.state.count < 20 ? (
+          <>
+            <h1>I love React this much:</h1>
+
+            <p className='heart-container'>
+              <Heart
+                size={20}
+                color={this.state.count > 9 ? "#fa5e8a" : "gray"}
+                style={{ marginRight: "20px", transform: `scale(${this.state.scale})` }}
+              />{" "}
+              Count: {this.state.count}
+            </p>
+
+            <button className='heart-increment' onClick={this.handleClick}>
+              More love
+            </button>
+          </>
+        ) : (
+          <div className='lots-of-love'>
+            <Smile size={30} color='#fa5e8a' /> <Smile size={30} color='#fa5e8a' /> <Smile size={30} color='#fa5e8a' />
+            <h1>Wow - that's a lot of love!!</h1>
+          </div>
+        )}
+      </main>
+    );
+  }
 }
 
 export default App;
